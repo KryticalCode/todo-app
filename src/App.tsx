8a100b6ab5ee61ad61ced54946ce6484020bcef6
KryticalCode <99172863+KryticalCode@users.noreply.github.com>
2025-07-02 23:26:24 +1000

    Add overflow-hidden to main element for improved layout handling

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
         Todo App
       </h1>
 
-      <main className="relative w-[972px] h-[636px] bg-white rounded-[8px] shadow-[0_4px_4px_rgba(0,0,0,0.08)] grid grid-cols[7fr_4fr] grid-rows-[59px_1fr]">
+      <main className="relative w-[972px] h-[636px] bg-white rounded-[8px] shadow-[0_4px_4px_rgba(0,0,0,0.08)] grid grid-cols[7fr_4fr] grid-rows-[59px_1fr] overflow-hidden">
         <header className="col-[1/3] row-[1/2] bg-[#fbf5ed] border-b border-black/[0.08]"></header>
 
         <ul></ul>
