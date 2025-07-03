ae545048ee20cf4859e40f3fce441a4353d4ad83
KryticalCode <mattcirona@gmail.com>
2025-07-03 14:16:18 +1000

    Fix: typo in Todo app title, main css typo

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ function App() {
       <h1 className="text-[120px] font-bold text-black/5 uppercase absolute left-1/2 -translate-x-1/2 tracking-[0.2em]">
         {/* absolute ensures the text does not push other elements down -- left-1/2 -translate-x-1/2 centers the text 
         horizontally by moving it left by 50% of its width and then translating it back by 50% of its width */}
-        Todo App
+        Todoapp
       </h1>
 
-      <main className="relative w-[972px] h-[636px] bg-white rounded-[8px] shadow-[0_4px_4px_rgba(0,0,0,0.08)] grid grid-cols[7fr_4fr] grid-rows-[59px_1fr] overflow-hidden">
+      <main className="relative w-[972px] h-[636px] bg-white rounded-[8px] shadow-[0_4px_4px_rgba(0,0,0,0.08)] grid grid-cols-[7fr_4fr] grid-rows-[59px_1fr] overflow-hidden">
         <header className="col-[1/3] row-[1/2] bg-[#fbf5ed] border-b border-black/[0.08]"></header>
 
         <ul></ul>
